dfc90320c3d36c3b77bfc8302a7e8113f5e2bec7
jdegand <70610011+jdegand@users.noreply.github.com>
2021-07-07 16:36:58 -0400

    Update FoodRanking.js

--- FoodRanking.js
+++ FoodRanking.js
@@ -1,3 +1,46 @@
+function setup(){
+    let upLink = Array.from(document.getElementsByTagName('button'))
+    .filter(el => el.textContent === 'Up!');
+    
+    for(let i = 0; i < upLink.length; i++){
+      upLink[i].addEventListener('click', function(){
+        let wrapper = this.parentElement;
+  
+        if(wrapper.previousElementSibling){
+          wrapper.parentNode.insertBefore(wrapper, wrapper.previousElementSibling)
+        }
+      })
+    }
+  
+    let downLink = Array.from(document.getElementsByTagName('button'))
+    .filter(el => el.textContent === 'Down!');;
+  
+    for(let i = 0; i < downLink.length; i++){
+      downLink[i].addEventListener('click', function(){
+        let wrapper = this.parentElement;
+  
+        if(wrapper.nextElementSibling){
+          wrapper.parentNode.insertBefore(wrapper.nextElementSibling, wrapper) 
+        }
+      })
+    }
+  }
+  
+  // Example case
+  document.body.innerHTML = `<ol>
+    <li><button>Up!</button>Taco<button>Down!</button></li>
+    <li><button>Up!</button>Pizza<button>Down!</button></li>
+    <li><button>Up!</button>Eggs<button>Down!</button></li>
+  </ol>`;
+  
+  setup();
+  document.getElementsByTagName('button')[2].click();
+  
+  console.log(document.body.innerHTML);
+
+
+
+/*
 //This solution relies on changing the HTML by adding classes of "up"/"down" on
 //the appropriate buttons
 
@@ -26,3 +69,4 @@ function setup(){
     })
   }
 }
+*/
