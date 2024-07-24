1f760a94f00bdf980275114c621dbdcaca282754
jdegand <70610011+jdegand@users.noreply.github.com>
2024-07-24 16:40:19 -0400

    fix: replace var in closure / loop

--- Closures.js
+++ Closures.js
@@ -1,5 +1,5 @@
 function registerHandlers(){
-  var as = document.getElementsByTagName('a');
+  const as = document.getElementsByTagName('a');
   for(let i = 0;  i < as.length; i++){
     as[i].onclick = function(){
       alert(i);
--- Loop.js
+++ Loop.js
@@ -1,8 +1,8 @@
 function appendChildren(decorateDivFunction){
-  var allDivs = [...document.getElementsByTagName('div')];
+  const allDivs = [...document.getElementsByTagName('div')];
 
   for(let i = 0; i < allDivs.length; i++){
-    var newDiv = document.createElement('div');
+    const newDiv = document.createElement('div');
     decorateDivFunction(newDiv);
     allDivs[i].appendChild(newDiv)
   }
